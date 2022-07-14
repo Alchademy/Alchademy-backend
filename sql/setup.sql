1afbb238e8185bacee055600e058bade0dc3f62a
Will Gundy <willgunderson503@gmail.com>
2022-07-14 15:12:45 -0700

    seed final assignment and submission data

--- sql/setup.sql
+++ sql/setup.sql
@@ -80,14 +80,11 @@ CREATE TABLE assignments (
   title TEXT,
   description TEXT,
   syllabus_id INT NOT NULL,
-  due_date TIMESTAMPTZ,
-  grade INT,
+  due_date TEXT,
   total_points INT,
   status_id INT NOT NULL,
-  user_id INT NOT NULL,
   FOREIGN KEY (status_id) REFERENCES status(id),
-  FOREIGN KEY (syllabus_id) REFERENCES syllabus(id),
-  FOREIGN KEY (user_id) REFERENCES users(id)
+  FOREIGN KEY (syllabus_id) REFERENCES syllabus(id)
 );
 
 CREATE TABLE submissions (
@@ -97,6 +94,7 @@ CREATE TABLE submissions (
   status_id INT NOT NULL,
   assignment_id INT NOT NULL,
   user_id INT NOT NULL,
+  grade INT,
   FOREIGN KEY (status_id) REFERENCES status(id),
   FOREIGN KEY (assignment_id) REFERENCES assignments(id),
   FOREIGN KEY (user_id) REFERENCES users(id)
@@ -124,7 +122,7 @@ INSERT INTO roles (name, description) VALUES
 ('Teacher', 'Leader of the cohort'),
 ('Admin', 'Full CRUD access across the application');
 
-INSERT INTO status (name) VALUES ('pending'), ('active'), ('archived');
+INSERT INTO status (name) VALUES ('pending'), ('active'), ('archived'), ('completed');
 
 INSERT INTO cohorts (month, year) VALUES
 ('February', 2022),
@@ -163,4 +161,30 @@ INSERT INTO cohort_to_syllabus (cohort_id, syllabus_id) VALUES
 (2, 3),
 (2, 4),
 (2, 5);
+
+INSERT INTO assignments (title, description, syllabus_id, due_date, total_points, status_id) VALUES
+('Half Baked: Soccer Score Keeper', '', 2, 'March 15th 2022', 10, 4),
+('From Scratch: Poll Maker', '', 2, 'March 17th 2022', 20, 4),
+('Half Baked: Mushroom Festival', '', 2, 'March 17th 2022', 10, 4),
+('From Scratch: Goblin Fighter', '', 2, 'March 18th 2022', 20, 4),
+('Half Baked: City Post Card Builder', '', 3, 'May 3rd 2022', 10, 4),
+('From Scratch: Fast Food Order App', '', 3, 'May 4th 2022', 20, 4),
+('Half Baked: Board Game Inventory', '', 3, 'May 12th 2022', 10, 4),
+('From Scratch: Pokedex Proxy', '', 3, 'May 18th 2022', 20, 4),
+('Half Baked: Lazy Bouncer Club', '', 5, 'June 22nd 2022', 10, 4),
+('From Scratch: Gitty', '', 5, 'June 17th 2022', 20, 4),
+('Half Baked: Cartoon Cats', '', 5, 'June 7th 2022', 10, 4),
+('From Scratch: Zodiac API', '', 5, 'June 8th 2022', 20, 4),
+('Half Baked: Dont Know Advanced React', '', 4, 'August 10th 2022', 10, 4),
+('From Scratch: Dont Know Advanced React', '', 4, 'August 11th 2022', 20, 4);
+
+INSERT INTO submissions (text, status_id, assignment_id, user_id, grade) VALUES 
+('Beau Submission for Soccer Score Keeper', 4, 1, 10, 10),
+('Beau Submission for Poll Maker', 4, 2, 10, 10),
+('Beau Submission for Mushroom Festival', 4, 3, 10, 6),
+('Beau Submission for Goblin Fighter', 1, 4, 10, null);
+
+
+
+
 
