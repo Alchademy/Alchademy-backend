1e389d3b5f393cdc6b5b727e9fb3603d1ee09ce0
Will Gundy <willgunderson503@gmail.com>
2022-07-14 10:52:25 -0700

    added test for deleting a user session

--- __tests__/githubUser.test.js
+++ __tests__/githubUser.test.js
@@ -35,6 +35,21 @@ describe('github routes', () => {
     });
   });
 
+  it('should remove user session on calling delete', async () => {
+    const agent = await request
+      .agent(app)
+      .get('/github/callback?code=55')
+      .redirects(1);
+    expect(agent.status).toEqual(200);
+
+    const loggedOut = await agent.delete('/github/sessions');
+
+    expect(loggedOut.body).toEqual({
+      message: 'Signed out successfully!',
+      success: true
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
