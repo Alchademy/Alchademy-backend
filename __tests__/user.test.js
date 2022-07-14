2ee8f4e1a129c198316aebd48e1cdf5550fc63cf
Beau Elliott <belliott1516@gmail.com>
2022-07-13 17:58:30 -0700

    added mocks to services and connected it to test

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -3,11 +3,13 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/services/github');
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('Get user', () => {
+  it('Get user from Github and inserts them into users table', () => {
     expect(1).toEqual(1);
   });
   afterAll(() => {
