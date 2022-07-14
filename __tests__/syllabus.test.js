946d62a08d320dba51a3f940b4ed23704ed8ca97
Beau Elliott <belliott1516@gmail.com>
2022-07-14 12:00:29 -0700

    added get route to syllabus controller

--- __tests__/syllabus.test.js
+++ __tests__/syllabus.test.js
@@ -13,7 +13,7 @@ describe('github routes', () => {
   it('GET /cohort/syllabus gets all courses for a students assigned cohort', async () => {
     const agent = await request.agent(app);
     await agent.get('/github/callback?code=55').redirects(1);
-    const res = await agent.get('/1/syllabus');
+    const res = await agent.get('/syllabus/1');
 
     expect(res.status).toEqual(200);
     expect(res.body.length).toEqual(1);
