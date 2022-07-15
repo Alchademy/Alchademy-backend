2552af661bb6a6e18253a6924f506cc21f23d283
Delaney Fogarty <delaney.fk@gmail.com>
2022-07-14 17:48:58 -0700

    still working on submissions testing

--- __tests__/submissions.test.js
+++ __tests__/submissions.test.js
@@ -4,35 +4,20 @@ const request = require('supertest');
 const app = require('../lib/app');
 const Submission = require('../lib/models/Submission');
 
+jest.mock('../lib/services/github.js');
+
 describe('backend submission routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
   it('GET /submissions returns a list of submissions associated with the authenticated user', async () => {
-    const res = await request(app).get('/submissions');
-    const allSubmissions = await Submission.getAllSubmissions();
-    const expected = allSubmissions.map((submission) => {
-      return {
-        id: submission.id,
-        created_on: submission.created_on,
-        status_id: submission.status_id,
-        assignment_id: submission.assignment_id,
-        user_id: submission.user_id,
-      };
-    });
-    expect(res.body).toEqual(expected);
-  });
-
-  it('GET /submissions/:id returns a single submission', async () => {
-    const res = await request(app).get('/submissions/3');
-    const beau_mushroom_fest = {
-      text: 'Beau Submission for Mushroom Festival',
-      status_id: 4,
-      assignment_id: 3,
+    const agent = await request.agent(app);
+    await agent.get('/github/callback?code=55').redirects(1);
+    const res = await agent.get('/submissions').send({
       user_id: 10,
-      grade: 6,
-    };
-    expect(res.body).toEqual(beau_mushroom_fest);
+    });
+    expect(res.status).toEqual(200);
+    expect(res.body.length).toEqual(4);
   });
 });
