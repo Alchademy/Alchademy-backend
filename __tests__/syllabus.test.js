f156bd162f2dac5e409cf02f293704b4918baf5d
Will Gundy <willgunderson503@gmail.com>
2022-07-15 15:10:27 -0700

    finished syllabus CRUD

--- __tests__/syllabus.test.js
+++ __tests__/syllabus.test.js
@@ -19,10 +19,10 @@ describe('github routes', () => {
     expect(res.body.length).toEqual(5);
   });
 
-  it('GET /cohort/syllabus gets all courses for a user by id', async () => {
+  it('GET /syllabus/users/:id gets all courses for a user by id', async () => {
     const agent = await request.agent(app);
     await agent.get('/github/callback?code=55').redirects(1);
-    const res = await agent.get('/syllabus/10');
+    const res = await agent.get('/syllabus/user/10');
 
     expect(res.status).toEqual(200);
     expect(res.body.length).toEqual(4);
@@ -41,6 +41,28 @@ describe('github routes', () => {
 
     expect(result.status).toEqual(200);
     expect(result.body.length).toEqual(6);
+  });
+
+  it('DELETE /syllabus/:id should delete a particular syllabus', async () => {
+    const agent = await request.agent(app);
+    await agent.get('/github/callback?code=55').redirects(1);
+    const resp = await agent.delete('/syllabus/1');
+    expect(resp.status).toEqual(200);
+    expect(resp.body.id).toEqual('1');
+    const res = await agent.get('/syllabus');
+    expect(res.body.length).toEqual(4);
+  });
+
+  it('POST /syllabus should update a particular syllabus', async () => {
+    const agent = await request.agent(app);
+    await agent.get('/github/callback?code=55').redirects(1);
+    const resp = await agent
+      .put('/syllabus/1')
+      .send({ title: 'test', thumbnail_photo: 'www.google.com' });
+    expect(resp.status).toEqual(200);
+    expect(resp.body.id).toEqual('1');
+    const res = await agent.get('/syllabus/1');
+    expect(res.body.title).toEqual('test');
   });
 
   afterAll(() => {
