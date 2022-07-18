a5c4275cee22a8674f5e1f9428fac6155cb41099
Beau Elliott <belliott1516@gmail.com>
2022-07-18 11:55:46 -0700

    added test to create a new ticket

--- __tests__/tickets.test.js
+++ __tests__/tickets.test.js
@@ -27,6 +27,21 @@ describe('assignments routes', () => {
     expect(res.body.length).toEqual(2);
   });
 
+  it('POST /ticket should create a single ticket on an assignment', async () => {
+    const agent = await request.agent(app);
+    await agent.get('/github/callback?code=55').redirects(1);
+    const res = await agent.post('/tickets').send({
+      title: 'From Scratch: Dont Know Advanced React',
+      description: '',
+      syllabus_id: 4,
+      due_date: 'August 11th 2022',
+      total_points: 20,
+      status_id: 4 
+    });
+    expect(res.status).toBe(200);
+    expect(res.body.length).toEqual(2);
+  });
+
   afterAll(() => {
     pool.end();
   });
