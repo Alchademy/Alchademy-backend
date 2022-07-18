6486f13a6ac31beec6d161701a56c88f33a68671
Beau Elliott <belliott1516@gmail.com>
2022-07-18 07:52:53 -0700

    added test to get all tickets currently in process

---
+++ __tests__/tickets.test.js
@@ -0,0 +1,26 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+jest.mock('../lib/services/github.js');
+
+describe('assignments routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('GET /tickets should display all tickets currently available ', async () => {
+    const agent = await request.agent(app);
+    await agent.get('/github/callback?code=55').redirects(1);
+    const res = await agent.get('/tickets');
+
+    expect(res.status).toEqual(200);
+    expect(res.body.length).toEqual(4);
+  });
+
+
+  afterAll(() => {
+    pool.end();
+  });
+});
