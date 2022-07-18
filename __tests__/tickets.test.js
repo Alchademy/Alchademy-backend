8c997c78658e2da3236867162183d1e705edda8b
Beau Elliott <belliott1516@gmail.com>
2022-07-18 12:46:04 -0700

    removed update from tickets.test.js

--- __tests__/tickets.test.js
+++ __tests__/tickets.test.js
@@ -2,7 +2,6 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const { update } = require('../lib/models/Assignment');
 
 jest.mock('../lib/services/github.js');
 
