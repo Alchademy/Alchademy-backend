9ff9a4c192dfc46d5f9f2d97e2b11352856f788b
Beau Elliott <belliott1516@gmail.com>
2022-07-19 11:25:19 -0700

    changed assignment controller with syllabus route to pull syllabus id and adjusted test

--- __tests__/assignments.test.js
+++ __tests__/assignments.test.js
@@ -13,19 +13,10 @@ describe('assignments routes', () => {
   it('GET /assignments should display all assignments for a syllabus', async () => {
     const agent = await request.agent(app);
     await agent.get('/github/callback?code=55').redirects(1);
-    const res = await agent.get('/assignments')
-      .send({
-        id: 2, 
-        title: 'Module 2: Web',
-        thumbnail_photo: 'url',
-        created_by: 1,
-        owner_id: 1,
-        description: '2nd module of Alchemy',
-        status: 1 
-      });
+    const res = await agent.get('/assignments/syllabus/1');
 
     expect(res.status).toEqual(200);
-    expect(res.body.length).toEqual(4);
+    expect(res.body.length).toEqual(0);
   });
 
   it('GET /assignments/:id should display a singular assignment', async () => {
